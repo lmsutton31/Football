9b922626544d05e7585d7cad78ecbf7912f0dd72
Luke Sutton <lmsutton31@gmai.com>
2026-01-18 14:23:54 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Luke Sutton on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
